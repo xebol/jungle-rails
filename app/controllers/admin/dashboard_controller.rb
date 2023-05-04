a0ad19d8dd43744dfd66b9d4d550666822732d53
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-03 19:29:17 -0600

    Authenticate admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate 
   def show
   end
-end
+  
+  private 
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
+      end
+    end
+  end
