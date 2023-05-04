d2acd4c06b11fcb93a48e1e2d9341030f7615188
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-03 21:02:04 -0600

    Show the product count and category count in the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,17 @@
 class Admin::DashboardController < ApplicationController
-  before_action :authenticate 
+  before_action :authenticate
+  def show; end
+
   def show
+    @inventory = Product.count
+    @category_count = Category.count
   end
-  
-  private 
-    def authenticate
-      authenticate_or_request_with_http_basic do |username, password|
-        username == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
-      end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
     end
   end
+end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  before_action :authenticate 
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
