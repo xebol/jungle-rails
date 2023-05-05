b36acfabcfa36a5a235514418531c451a9ec3860
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-04 20:45:02 -0600

    Add a simple user authentication to the app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   resources :products, only: %i[index show]
   resources :categories, only: [:show]
+  resources :users, only: %i[create new]
+  resources :sessions, only: %i[new create destroy]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -17,6 +23,9 @@ Rails.application.routes.draw do
     resources :products, except: %i[edit update show]
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
