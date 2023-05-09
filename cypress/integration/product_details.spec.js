02377242f8652adc9acbdf41d64e27c48beca4ec
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-09 16:59:51 -0600

    Test that a user can click on a product to see product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -10,4 +10,10 @@ describe('Visit the home page', () => {
   it("There are 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
-})
+
+  it('.click() - navigates from the homepage to the product detail page by clicking on a product', () => {
+    // cy.get('.products article').click()
+    cy.get('.products article').first().click()
+    cy.get(".product-detail").should("be.visible")
+  })
+  })
